28416fc9a5265df7abbfbca4d57619b953890a2e
AliyaKad <aliyakadirova.05@mail.ru>
2025-04-27 18:51:43 +0300

    сделан экран с созданием нового пароля

--- app/src/main/java/com/example/t_bank/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/t_bank/ResetPasswordFragment.kt
@@ -1,10 +1,51 @@
 package com.example.t_bank
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.t_bank.databinding.FragmentResetPasswordBinding
 
-class ResetPasswordFragment : Fragment(R.layout.fragment_reset_password) {
+class ResetPasswordFragment : Fragment() {
+
+    private var _binding: FragmentResetPasswordBinding? = null
+
+    private val binding get() = requireNotNull(_binding) { "Binding is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupClickListeners() {
+        binding.btnConfirm.setOnClickListener {
+            onConfirmClicked()
+        }
+    }
+
+    private fun onConfirmClicked() {
+
+        val password = binding.tPassword.editText?.text.toString()
+        val confirmPassword = binding.tlConfirmPassword.editText?.text.toString()
+
+        if (password == confirmPassword && password.isNotEmpty()) {
+        } else {
+
+        }
+    }
 }
