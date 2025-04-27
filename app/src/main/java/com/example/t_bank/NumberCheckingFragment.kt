414d333ba0f75fa67dca8fbf8f674e3864387f1d
AliyaKad <aliyakadirova.05@mail.ru>
2025-04-27 18:39:50 +0300

    сделаны экраны с входом и регистрацией

--- app/src/main/java/com/example/t_bank/NumberCheckingFragment.kt
+++ app/src/main/java/com/example/t_bank/NumberCheckingFragment.kt
@@ -1,7 +1,48 @@
 package com.example.t_bank
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.t_bank.databinding.FragmentNumberCheckingBinding
 
-class NumberCheckingFragment: Fragment(R.layout.fragment_number_checking) {
+class NumberCheckingFragment : Fragment() {
+    private var _binding: FragmentNumberCheckingBinding? = null
 
+    private val binding get() = requireNotNull(_binding) { "Binding is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNumberCheckingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupClickListeners() {
+        binding.btnSubmit.setOnClickListener {
+            onSubmitClicked()
+        }
+
+        binding.tvResendCode.setOnClickListener {
+            onResendCodeClicked()
+        }
+    }
+
+    private fun onSubmitClicked() {
+    }
+
+    private fun onResendCodeClicked() {
+    }
 }
--- app/src/main/java/com/example/t_bank/PasswordCreatingFragment.kt
+++ app/src/main/java/com/example/t_bank/PasswordCreatingFragment.kt
@@ -1,6 +1,50 @@
 package com.example.t_bank
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.t_bank.databinding.FragmentPasswordCreatingBinding
 
-class PasswordCreatingFragment: Fragment(R.layout.fragment_password_creating) {
+class PasswordCreatingFragment : Fragment() {
+
+    private var _binding: FragmentPasswordCreatingBinding? = null
+
+    private val binding get() = requireNotNull(_binding) { "Binding is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPasswordCreatingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupClickListeners() {
+        binding.btnConfirm.setOnClickListener {
+            onConfirmClicked()
+        }
+    }
+
+    private fun onConfirmClicked() {
+
+        val password = binding.tPassword.editText?.text.toString()
+        val confirmPassword = binding.tlConfirmPassword.editText?.text.toString()
+
+        if (password == confirmPassword && password.isNotEmpty()) {
+        } else {
+        }
+    }
 }
